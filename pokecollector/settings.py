80f131a1b0b36d7c47a80c7fb552372e92088f7f
JP_Escobari <jpe911@gmail.com>
2024-09-26 12:00:07 -0400

    Pokecollector Part1 Complete

--- pokecollector/settings.py
+++ pokecollector/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
+import environ
+environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,11 +82,14 @@ WSGI_APPLICATION = 'pokecollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'pokecollector',
+      'USER': os.environ['DB_USER'],
+      'PASSWORD': os.environ['DB_PW'],
+      'HOST': os.environ['DB_HOST'],
+      'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
