ec7c7ef804f80312c26082d3b2b505af0bcb5133
ian-ruiz21 <ruiz.ian21@gmail.com>
2024-10-01 13:33:41 -0500

    Refactor

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    xp = models.IntegerField()
+    type = models.CharField(max_length=100)
+    description = models.TextField()
+    image_url = models.URLField()
+
+    def __str__(self):
+        return self.name
