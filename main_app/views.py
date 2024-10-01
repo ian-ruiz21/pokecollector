8209639374539e8a180171b4d86d17d6e2ea4fba
ian-ruiz21 <ruiz.ian21@gmail.com>
2024-10-01 07:43:28 -0500

    Complete Part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Pokemon:
+    def __init__(self, name, type_, description, level):
+        self.name = name
+        self.type_ = type_
+        self.description = description
+        self.level = level
+
+pokemon = [
+    Pokemon('Pikachu', 'electric', 'Kinda rude.', 3),
+    Pokemon('Bulbasaur', 'grass/poison', 'Looks like a turtle.', 0),
+    Pokemon('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Pokemon('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'about.html')
+
+def pokemon_index(request):
+    return render(request, 'pokemon/index.html', { 'pokemon': pokemon })
